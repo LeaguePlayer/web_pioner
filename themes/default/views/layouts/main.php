eb04e0a99833e687a734db9ba8e2bc3476ffb515
megakuzmitch <maxro.kuzmitch@gmail.com>
2014-06-25 15:26:41 +0600

    update scripts

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -2,7 +2,7 @@
 	$cs = Yii::app()->clientScript;
 	$assetsUrl = $this->getAssetsUrl();
 	$cs->registerCssFile($assetsUrl.'/css/normalize.min.css');
-	$cs->registerCssFile($assetsUrl.'/css/main.css?v=10');
+	$cs->registerCssFile($assetsUrl.'/css/main.css?v=11');
 
 	$cs->registerCoreScript('jquery');
 	$cs->registerCoreScript('jquery.ui');
